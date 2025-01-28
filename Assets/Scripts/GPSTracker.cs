02371ac9c8628b344f039a79ceb77766fa67cf85
Clay Jones <122416165+clay-creates@users.noreply.github.com>
2025-01-28 14:30:04 -0600

    updated scripts for GPS info

--- Assets/Scripts/GPSTracker.cs
+++ Assets/Scripts/GPSTracker.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Niantic.Lightship.AR;
-using Niantic.Lightship.AR.WorldPositioning;
 using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class GPSTracker : MonoBehaviour
 {
-    ARWorldPositioningCameraHelper cameraHelper;
-
     private double currentLongitude;
     private double currentLatitude;
     private double currentAltitude;
@@ -17,26 +16,106 @@ public class GPSTracker : MonoBehaviour
     public TMP_Text latitudeText;
     public TMP_Text altitudeText;
 
+    private float updateInterval = 1f;
+    private float timer = 0f;
+
+    public Button saveCoordButton;
+    public Button requestCoordButton;
+    public Button calculateDistanceButton;
+
+    private string currentCoordinates = "";
+    private string requestedCoordinates = "";
+    private string distanceBetweenCoords = "";
+
+    public TMP_Text requestedCoordinatesText;
+    public TMP_Text savedCoordinatesText;
+    public TMP_Text distanceBetweenCoordsText;
+
     // Start is called before the first frame update
     void Start()
     {
-        cameraHelper = Camera.main.GetComponent<ARWorldPositioningCameraHelper>();
+        StartCoroutine(InitializeLocationService());
+
+        saveCoordButton.onClick.AddListener(SaveCurrentCoords);
+        requestCoordButton.onClick.AddListener(RequestNewCoords);
+        calculateDistanceButton.onClick.AddListener(CalculateDistanceBetweenCoords);
+    }
+
+    private IEnumerator InitializeLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("Location Service is disabled by the user.");
+            yield break;
+        }
+
+        Input.location.Start();
+
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            Debug.LogError("Location service initialization timed out.");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.LogError("Unable to determine device location.");
+            yield break;
+        }
+
+        Debug.Log("Location service initialized successfully.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentLongitude = cameraHelper.Longitude;
-        currentLatitude = cameraHelper.Latitude;
-        currentAltitude = cameraHelper.Altitude;
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            timer += Time.deltaTime;
 
-        UpdateGPSText();
+            if (timer >= updateInterval)
+            {
+                LocationInfo locationInfo = Input.location.lastData;
+                currentLongitude = locationInfo.longitude;
+                currentLatitude = locationInfo.latitude;
+                currentAltitude = locationInfo.altitude;
+
+                UpdateGPSText();
+                timer = 0f;
+            }
+        }
+        else
+        {
+            Debug.Log("Waiting for location service...");
+        }
     }
 
     public void UpdateGPSText()
     {
-        longitudeText.text = currentLongitude.ToString();
-        latitudeText.text = currentLatitude.ToString();
-        altitudeText.text = currentAltitude.ToString();
+        longitudeText.text = currentLongitude.ToString("F6");
+        latitudeText.text = currentLatitude.ToString("F6");
+        altitudeText.text = currentAltitude.ToString("F2");
+    }
+
+    public void SaveCurrentCoords()
+    {
+
+    }
+
+    public void RequestNewCoords()
+    {
+
+    }
+
+    public void CalculateDistanceBetweenCoords()
+    {
+
     }
 }
